b1d6eb313c05aec7cad8d4e08e04ff7af0d76b79
sidamarnath <75329890+sidamarnath@users.noreply.github.com>
2024-08-24 17:57:41 -0400

    Add files via upload

---
+++ arewethereyetamarnat4/app/src/main/java/edu/msu/amarnat4/arewethereyetamarnat4/MapActivity.java
@@ -0,0 +1,14 @@
+package edu.msu.amarnat4.arewethereyetamarnat4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
