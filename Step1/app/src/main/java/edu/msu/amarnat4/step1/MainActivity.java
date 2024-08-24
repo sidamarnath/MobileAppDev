71c5fdb30823fc55db5ed368538b1fd1181ef67c
sidamarnath <75329890+sidamarnath@users.noreply.github.com>
2024-08-24 17:46:11 -0400

    Add files via upload

---
+++ Step1/app/src/main/java/edu/msu/amarnat4/step1/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.msu.amarnat4.step1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
